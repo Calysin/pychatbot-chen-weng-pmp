fcdfe90d6644b107ab8f51aee6abe3832dfbc1ac
Calysin <chenirene1006@gmail.com>
2023-12-03 23:14:02 +0100

    modification du menu

--- main.py
+++ main.py
@@ -13,37 +13,33 @@ print_noms(prenom_n)
 ConversionMajusculeEnMinuscule(files_names)
 del_ponctuations(files_names)
 print("\nVoici les fonctionnalités que nous disposons pour le moment : \n")
-print("1 - Afficher la liste des mots les moins importants dans le corpus document")
-print("2 - Afficher le mot ayant le score TD-IDF le plus élevé")
-print("3 - Afficher le mot le plus répéter par un président")
-print("4 - Afficher la liste des présidents qui ont parlé de la (Nation) et celui qui l'a répété le plus de fois")
-print("5 - Afficher le premier présidant ayant parlé de climat et d'écologie")
-print("5 - Afficher le premier présidant ayant parlé d'écologie")
-print("6 - Afficher les mots que tous les présidents ont évoqués \n")
+menu = print("1 - Afficher la liste des mots les moins importants dans le corpus document \n"
+             "2 - Afficher le mot ayant le score TD-IDF le plus élevé \n"
+             "3 - Afficher le mot le plus répéter par un président\n"
+             "4 - Afficher la liste des présidents qui ont parlé de la (Nation) et celui qui l'a répété le plus de fois\n"
+             "5 - Afficher le premier présidant ayant parlé de climat et d'écologie\n"
+             "6  - Afficher les mots que tous les présidents ont évoqués \n")
 
 print("Pour accéder aux fonctionnalités, veuillez écrire les numéros auxquels elles correspondent")
-functionality = input("A quelle fonctionnalité voulez-vous accéder ? ")
 
-if functionality == "1":
-    tf_idf, liste_mot = (TF_IDF(files_names))
-if functionality == "2":
-    print("Le mot avec le tf-idf le plus élevé de tout les discours est: ", tf_idf_max(files_names))
-if functionality == "3":
-    nom = input("Entrer le nom d'un president dont vous souhaitez connaître le mot le plus répéter dans ses discours : ")
-    mot_plus_repet(nom)
-if functionality == "4":
-    print("Les présidents ayant dit nation sont:", ResultatNation(files_names))
-    print("Le président l'ayant le plus dit est:", ResultatNation(files_names)[0])
-if functionality == "5":
-    print("Le premier président ayant dit climat est", PremierClimat(files_names))
-if functionality == "5":
-    print("Le premier présidant ayant dit écologie est", PremierEcologie(files_names))
-if functionality == "6":
-    print("Celui-ci est encore en cours de developpement")
+n = int(input("Combien de fonctionnalités voulez-vous tester ? "))
 
-
-
-
-
-
-
+for i in range (n):
+    functionality = input("A quelle fonctionnalité voulez-vous accéder ? ")
+    if functionality == "1":
+        print("Celui-ci est en cours de developpement")
+    if functionality == "2":
+        print("Le mot avec le tf-idf le plus élevé de tout les discours est: ", tf_idf_max(files_names))
+    if functionality == "3":
+        nom = input("Entrer le nom d'un president dont vous souhaitez connaître le mot le plus répéter dans ses discours : ")
+        mot_plus_repet(nom)
+    if functionality == "4":
+        print("Les présidents ayant dit nation sont:", ResultatNation(files_names))
+        print("Le président l'ayant le plus dit est:", ResultatNation(files_names)[0])
+    if functionality == "5":
+        print("Le premier président ayant dit climat est", PremierClimat(files_names))
+        print("Le premier présidant ayant dit écologie est", PremierEcologie(files_names))
+    if functionality == "6":
+        print("Celui-ci est encore en cours de developpement")
+    if functionality >= "7":
+        print("Nous ne faisons pas autant de fonctionnalité pour le moment")
