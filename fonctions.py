1fb2cebd9e0aa82e1812219d710690f178229e77
Calysin <chenirene1006@gmail.com>
2023-12-03 20:36:34 +0100

    Mot Nation

--- fonctions.py
+++ fonctions.py
@@ -108,7 +108,6 @@ def TF(ch):
             if list_mot[i]==mot:    #si la valeur de la liste est mot
                 repet+=1            #incremente repet
         dictionnaire[list_mot[i]]=repet #dictionnaire de la valeur de la liste vaut repet
-
     return dictionnaire
 
 def IDF(L): #Fonction calculant l'IDF
@@ -130,114 +129,81 @@ def IDF(L): #Fonction calculant l'IDF
 
 def transpose_matrice(M):
     matrice_final=[]
-
     for j in range(len(M[0])):          #boucle range la plus grande longueur de ligne
         L=[]
         for i in range(len(M)):     #boucle range longueur de la matrice
             L.append(M[i][j])   #on apprend
-
         matrice_final.append(L)
-
     return matrice_final
 
-
 def TF_IDF(files_names):
-
     liste_nom_numero = extraire_noms_avec_numero(files_names)  #liste de tout les noms avec numero
     matrice_tf_idf = []
     mot_tf = []
-
     for i in range(len(liste_nom_numero)):  # Boucle permettant de parcourir tous les fichiers, et de calculer chaque tf-idf d'un mot dans un fichier
-
         with open('cleaned/CleanedNomination_{}.txt'.format(liste_nom_numero[i]), 'r', encoding="utf-8") as f:
             contenu = f.read()
-
         tf = TF(contenu)
-
         for mot in tf:              #en dehors de la 2eme i boucle pour éviter que la premiere colonne ne correspond pas au fichier
             if mot not in mot_tf:
                 mot_tf.append(mot)  #liste contenant tt les mots sans doublon et gardant le meme ordre
-
-
     for i in range(len(liste_nom_numero)):  # Boucle permettant de parcourir tous les fichiers, et de calculer chaque tf-idf d'un mot dans un fichier
         with open('cleaned/CleanedNomination_{}.txt'.format(liste_nom_numero[i]), 'r', encoding="utf-8") as f:
             contenu=f.read()
-
         tf = TF(contenu)        #tf de ce fichier
         idf = IDF(files_names)  #idf de tt les fichiers
-
         L=[]
-
         for mot in mot_tf:
-
             if mot in tf:
                 L.append((tf[mot] * idf[mot]))  #apprend le tf-idf de chaque mot de ce fichier
             else:
                 L.append(0)                     #si le mot n'existe pas dans ce fichier = 0
-
         matrice_tf_idf.append(L)            #matrice, ligne: fichier, colonne: tf-idf de chaque mot du fichier
-
     tf_idf=transpose_matrice(matrice_tf_idf)    #transpose matrice pour avoir les lignes et les colonnes inversé
-
     return tf_idf, mot_tf
 
 def tf_idf_max(files_names):            #retourne dictionnaire des mots avec les plus grans tf-idf
-
     tf_idf, liste_mot = TF_IDF(files_names)     #prend la matrice if-idf et la liste des mots, chaque ligne des 2 corresponds au même mot
-
     tmp_val = 0
     for i in range(8):
         tmp_val += tf_idf[0][i]              #initialise une valeur a tt ces variables pour pouvoir trouver le max
     mot = [liste_mot[0]]
-
     tfidf_max=[]
     tfidf_max.append(tmp_val)                  #liste apprend cette valeur
-
     for i in range(len(tf_idf)):
         somme_tfidf=0
-
         for j in range(len(tf_idf[i])):
             somme_tfidf += tf_idf[i][j]                 #calcul somme tf-idf d'un mot
-
         if tmp_val<somme_tfidf:                       #si cette somme est plus grande que tfidf_max
             tfidf_max=[]
             tfidf_max.append(somme_tfidf)               #tfidf_max devient cette somme
             tmp_val=somme_tfidf
-
             mot=[]
             mot.append(liste_mot[i])                         #on stock le mot correspondant
-
         elif tfidf_max==somme_tfidf: #apprendre une autre variable dans la liste si elle a autant de repetition que le(s) mot(s) deja stocker
             tfidf_max.append(somme_tfidf)  # tfidf_max devient cette somme
             mot.append(liste_mot[i])
-
     detail_max={}
     val=0
     for m in mot:
         detail_max[m]=tfidf_max[val]   #dico avec le plus grand tf-idf et le mot correspondant
         val+=1
-
     return detail_max
 
 def tf_idf_0(files_names):
     tf_idf, liste_mot = TF_IDF(files_names) # prend la matrice if-idf et la liste des mots, chaque ligne des 2 corresponds au même mot
-
     L_mot_non_important=[]                  #creer liste qui va stocker les mots les moins important
     for i in range(len(tf_idf)):
         fichier_8=0
         for j in range(len(tf_idf[i])):
-
             if round(tf_idf[i][j])==0:
                 fichier_8 += 1              #si le tf-idf du mot dans un fichier est eagal à 0 on incremente la variables fichier8
-
         if fichier_8==8:                    #si fichier8 == 8 est donc que tt les tf-idf d'un mot dans chaque fichier vaut 0
             L_mot_non_important.append(liste_mot[i])    #la liste apprend le mot correspondant
-
     return L_mot_non_important
 
 def mot_plus_repet(nom):
     list_nom=[]
-
     if nom=='Chirac' or nom=='Mitterrand':  #si c chirac ou mitterrand on prend en compte le fait qu'ils ont 2 discours
         nom1 = nom+'1'
         nom2 = nom+'2'
@@ -245,30 +211,55 @@ def mot_plus_repet(nom):
         list_nom.append(nom2)
     else:
         list_nom.append(nom)
-
     dictionnaire={}
     for i in range(len(list_nom)):  # Boucle permettant de parcourir les fichiers
         with open('cleaned/CleanedNomination_{}.txt'.format(list_nom[i]), 'r', encoding="utf-8") as f:
             contenu=f.read()
         tf = TF(contenu)            #obtient dictionnaire du fichier
-
         for mot in tf:
             if mot in dictionnaire:
                 dictionnaire[mot] += tf[mot]    #si le mot est dans dictionnaire, on ajouter juste la valeur d'occurence du mot à celle existante
             else:
                 dictionnaire[mot] = tf[mot]     #sinon dictionnaire apprend ce mot et sa valeur
-
     liste_mot=[mot]             #prend un mot et sa valeur au pif pour pouvoir comparer
     mot_plus=dictionnaire[mot]
-
     for mot in dictionnaire:
         if dictionnaire[mot] > mot_plus:    #si le mot à une plus grande valeur que celle stoker
             liste_mot=[]                    #on supprime ceux stocker
             liste_mot.append(mot)           #et on apprend le nouveau mot et sa valeur
             mot_plus = dictionnaire[mot]
-
         elif dictionnaire[mot] == mot_plus: #apprendre une autre variable dans la liste si elle a autant de repetition que le(s) mot(s) deja stocker
             liste_mot.append(mot)
-
     print(liste_mot)
+
+def Nation(L):
+    DicoPresidentNation = {}
+    ListeNomNum = extraire_noms_avec_numero(L)
+    ListeNom = []
+    for k in range(len(ListeNomNum)):
+        DicoPresidentNation[ListeNomNum[k]] = 0
+    for i in range(len(ListeNomNum)):  # Boucle permettant de parcourir les fichiers
+        compteurNation = 0
+        with open('cleaned/CleanedNomination_{}.txt'.format(ListeNomNum[i]), 'r') as f:
+            contenu = f.read()
+            compteurNation = contenu.count("nation")
+            DicoPresidentNation[ListeNomNum[i]] = compteurNation
+    DicoPresidentNation["Chirac"] = DicoPresidentNation["Chirac1"] + DicoPresidentNation["Chirac2"]
+    DicoPresidentNation.pop("Chirac1")
+    DicoPresidentNation.pop("Chirac2")
+    DicoPresidentNation["Mitterand"] = DicoPresidentNation["Mitterrand1"] + DicoPresidentNation["Mitterrand2"]
+    DicoPresidentNation.pop("Mitterrand1")
+    DicoPresidentNation.pop("Mitterrand2")
+    for item in DicoPresidentNation:
+        ListeNom.append(item)
+    for j in range(len(ListeNom)):
+        if DicoPresidentNation[ListeNom[j]] == 0:
+            DicoPresidentNation.pop(ListeNom[j])
+    return sorted(DicoPresidentNation.items(), key = lambda x:x[1], reverse=True)
+
+def ResultatNation(L):
+    ListePresidentNation = []
+    for i, j in Nation(L):
+        ListePresidentNation.append(i)
+    return ListePresidentNation
 
--- main.py
+++ main.py
@@ -16,5 +16,7 @@ tf_idf, liste_mot = (TF_IDF(files_names))
 
 print("Le mot avec le tf-idf le plus élevé de tout les discours est: ", tf_idf_max(files_names))
 
-nom=str(input("Entrer le nom d'un president dont vous souhaitez connaître le mot le plus répéter dans ses discours : "))
-mot_plus_repet(nom)
+
+
+print("Les présidents ayant dit Nation sont:", ResultatNation(files_names))
+print("Le président l'ayant le plus dit est:", ResultatNation(files_names)[0])
