1dbfd0c2baf3335d04b3981111f08910bb935ece
Irène Chen <151174690+Calysin@users.noreply.github.com>
2023-12-22 09:39:58 +0100

    Calcul de similarité (#26)

--- fonctions.py
+++ fonctions.py
@@ -1,6 +1,6 @@
 #Import des bibliothèques
 import os
-import math
+from math import *
 def list_of_files(directory, extension): #Code donné pour le projet permettant d'obtenir les fichiers
     files_names = []
     for filename in os.listdir(directory):
@@ -331,7 +331,6 @@ def CleanedQuestion(char):
                 StockageLettre = lettre
                 mot += StockageLettre
         LQuestionPropre.append(mot)
-
     LQuestionPropre2 = []
     for mot in LQuestionPropre:
         mot2 = ""
@@ -340,7 +339,6 @@ def CleanedQuestion(char):
                 mot2 += lettre
         if mot2:
             LQuestionPropre2.append(mot2)
-
     return LQuestionPropre2
 
 
@@ -386,3 +384,31 @@ def TF_IDF_question(question, files_names):
             tf_idf.append(0)
 
     return tf_idf
+
+def CalculSimilaritéAProduitScalaire(AM, BL):
+    ProduitScalaireAB = 0
+    for i in range(len(AM)):
+        for j in range(len(AM[i])):
+            ProduitScalaireAB += AM[i][j] * BL[j]
+    return ProduitScalaireAB
+
+
+def CalculSimilaritéBNormeVecteur(A):
+    NormeA = 0
+    if isinstance(A, list) and isinstance(A[0], list) == True:
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                NormeA += A[i][j] ** 2
+        NormeA = sqrt(NormeA)
+    else:
+        for i in range(len(A)):
+            NormeA += A[i]**2
+        NormeA = sqrt(NormeA)
+    return NormeA
+
+def CalculSimilaritéCFinal(AM, BL):
+    ProduitScalaireAB = CalculSimilaritéAProduitScalaire(AM, BL)
+    NormeAM = CalculSimilaritéBNormeVecteur(AM)
+    NormeBL = CalculSimilaritéBNormeVecteur(BL)
+    ScoreSimilarité = ProduitScalaireAB / (NormeAM * NormeBL)
+    return ScoreSimilarité
