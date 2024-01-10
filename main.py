9b3fcacf3c552dd406d6fcab466d8bac9bc4d4d3
Calysin <chenirene1006@gmail.com>
2024-01-10 17:57:31 +0100

    Calcul de similarité (modification)

--- main.py
+++ main.py
@@ -5,16 +5,6 @@ directory = "./speeches"
 files_names = list_of_files(directory, "txt")
 #fils names correspond a une liste contenant tous les noms de fichiers
 
-Question = input("Donnez moi votre question ? ")
-CleanedQuestion(Question)
-
-test = print("test", TF_IDF_question(Question, files_names))
-tes = print("tes", calcul_doc_plus_pert(Question, files_names))
-te = print("te", TFIDFQuestionPlusElevee(Question, files_names))
-t = print("t", contenu_doc_plus_imp(te))
-
-print("\n")
-print("\n")
 print("Bienvenue dans le pychatbot ! \n")
 print("Nos informations concernent les discours de ces présidents: ")
 noms = (extraire_noms_presidents(files_names))
@@ -28,20 +18,22 @@ menu = print("1 - Afficher la liste des mots les moins importants dans le corpus
              "3 - Afficher le mot le plus répéter par un président\n"
              "4 - Afficher la liste des présidents qui ont parlé de la (Nation) et celui qui l'a répété le plus de fois\n"
              "5 - Afficher le premier présidant ayant parlé de climat et d'écologie\n"
-             "6  - Afficher les mots que tous les présidents ont évoqués \n")
-
+             "6 - Afficher les mots que tous les présidents ont évoqués \n"
+             "7 - Notre PyChatBot")
 print("Pour accéder aux fonctionnalités, veuillez écrire les numéros auxquels elles correspondent")
 
 recommencer=1
 while recommencer==1:
     functionality = input("A quelle fonctionnalité voulez-vous accéder ? ")
     if functionality == "1":
-        print("Celui-ci est en cours de developpement")
+        print("Les mots les moins importants dans les textes sont: ")
+        print(tf_idf_0(files_names))
     elif functionality == "2":
         print("Le mot avec le tf-idf le plus élevé de tout les discours est: ", tf_idf_max(files_names))
     elif functionality == "3":
-        nom = input("Entrer le nom d'un president dont vous souhaitez connaître le mot le plus répéter dans ses discours : ")
-        mot_plus_repet(nom)
+        print("Merci d'écrire le nom du président avec une majuscule au début")
+        nom = input("Entrez le nom d'un président dont vous souhaitez connaître le mot le plus souvent répété dans ses discours: ")
+        mot_plus_repet(nom, files_names)
     elif functionality == "4":
         print("Les présidents ayant dit nation sont:", ResultatNation(files_names))
         print("Le président l'ayant le plus dit est:", ResultatNation(files_names)[0])
@@ -51,7 +43,10 @@ while recommencer==1:
     elif functionality == "6":
         print("Celui-ci est encore en cours de developpement")
     elif functionality >= "7":
-        print("Nous ne faisons pas autant de fonctionnalité pour le moment")
+        Question = input("Donnez moi votre question ? ")
+
+        test = calcul_doc_plus_pert(Question, files_names)
+        t = print("t", contenu_doc_plus_imp(test))
 
     print("Souhaitez vous utiliser d'autre fonctionnalité?")
     recommencer=int(input("0 : Non\n"
